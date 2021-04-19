731cf0b9904f3002276344046bdc862f6bcb797d
sl <sunlei@boya-triz.com>
2021-04-19 21:43:13 +0800

    添加组合模式

---
+++ CompsitePattern/OrganizationBase.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompsitePattern
+{
+    /// <summary>
+    /// 组织机构的基类 
+    /// </summary>
+    public abstract class OrganizationBase
+    {
+        private string _name;
+
+        protected List<OrganizationBase> Organizations = new List<OrganizationBase>();
+
+        public OrganizationBase(string name)
+        {
+            _name = name;
+        }
+
+        /// <summary>
+        /// 增加子节点
+        /// </summary>
+        /// <param name="organization"></param>
+        public abstract void AddChildren(OrganizationBase organization);
+
+        /// <summary>
+        /// 删除子节点
+        /// </summary>
+        /// <param name="organization"></param>
+        public abstract void RemoveChildren(OrganizationBase organization);
+
+        public virtual void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + _name);
+        }
+    }
+
+    /// <summary>
+    /// 总部
+    /// </summary>
+    public class HeadOrganization : OrganizationBase
+    {
+        public HeadOrganization(string name) : base(name)
+        {
+        }
+
+        public override void AddChildren(OrganizationBase organization)
+        {
+            this.Organizations.Add(organization);
+        }
+
+        public override void RemoveChildren(OrganizationBase organization)
+        {
+            this.Organizations.Remove(organization);
+        }
+
+
+        public override void Display(int depth)
+        {
+            base.Display(depth);
+            ++depth;
+
+            foreach (var organization in base.Organizations)
+            {
+                organization.Display(depth);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 总部分部
+    /// </summary>
+    public class HeadBranchOrganization : OrganizationBase
+    {
+        public HeadBranchOrganization(string name) : base(name)
+        {
+        }
+
+        public override void AddChildren(OrganizationBase organization)
+        {
+            this.Organizations.Add(organization);
+        }
+
+        public override void Display(int depth)
+        {
+            base.Display(depth);
+            ++depth;
+
+            foreach (var organization in base.Organizations)
+            {
+                organization.Display(depth);
+            }
+        }
+
+        public override void RemoveChildren(OrganizationBase organization)
+        {
+            this.Organizations.Remove(organization);
+        }
+    }
+
+    /// <summary>
+    /// 总部分部的城市分部
+    /// </summary>
+    public class HeadBranchCityOrganization : OrganizationBase
+    {
+        public HeadBranchCityOrganization(string name) : base(name)
+        {
+        }
+
+        public override void AddChildren(OrganizationBase organization)
+        {
+            this.Organizations.Add(organization);
+        }
+
+        public override void Display(int depth)
+        {
+            base.Display(depth);
+        }
+
+        public override void RemoveChildren(OrganizationBase organization)
+        {
+            this.Organizations.Remove(organization);
+        }
+    }
+}
---
+++ CompsitePattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CompsitePattern
+{
+    /// <summary>
+    /// 组合模式  
+    /// 将对象组合成树形结构以表示 部分-整体的层次结构 组合模式使得用户对单个对象和组合对象的使用具有一致性
+    /// 
+    /// 对于组合模式的树形结构  父类中的Add Remove方法是为了控制自己本身拥有的子节点
+    /// 
+    /// 但是对于枝节点 和叶节点来说  叶节点本身是不需要Add Remove方法的 如果枝节点 和叶节点所继承的父类拥有Add Remove方法 
+    /// 那么这就是一个透明模式 对于外界来说更加方便不用区分枝和叶
+    /// 但是如果枝节点 和叶节点所继承的父类没有Add Remove方法  而枝节点中却实现了Add Remove方法 那么这就是安全模式，客户端需要
+    /// 知道具体哪个是枝节点哪个叶节点 反而使的客户端调用时比较麻烦
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrganizationBase rootOrganization = new HeadOrganization("总部基地");
+
+            //总部的南北总分部
+            OrganizationBase southOrganization = new HeadOrganization("南方分部");
+            OrganizationBase northOrganization = new HeadOrganization("北方分部");
+            rootOrganization.AddChildren(southOrganization);
+            rootOrganization.AddChildren(northOrganization);
+
+            //南方分部的子部门
+            OrganizationBase shangHaiOrganization = new HeadOrganization("南方上海分部");
+            OrganizationBase wuHanOrganization = new HeadOrganization("南方武汉分部");
+            OrganizationBase guangZhouOrganization = new HeadOrganization("南方广州分部");
+            southOrganization.AddChildren(shangHaiOrganization);
+            southOrganization.AddChildren(wuHanOrganization);
+            southOrganization.AddChildren(guangZhouOrganization);
+
+            //北方方分部的子部门
+            OrganizationBase shanxiOrganization = new HeadOrganization("北方山西分部");
+            OrganizationBase tianJinOrganization = new HeadOrganization("北方天津分部");
+            northOrganization.AddChildren(shanxiOrganization);
+            northOrganization.AddChildren(tianJinOrganization);
+
+            rootOrganization.Display(1);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
