7e1c43ae0fdc26d162636ac5645f632efe87ecf3
sl <sunlei@boya-triz.com>
2021-04-20 21:39:53 +0800

    添加迭代器模式

---
+++ IteratorPattern/Iterator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorPattern
+{
+    public interface Iterator
+    {
+        /// <summary>
+        /// 第一个元素
+        /// </summary>
+        /// <returns></returns>
+        object First();
+
+        /// <summary>
+        /// 最后一个元素
+        /// </summary>
+        /// <returns></returns>
+        object Last();
+
+        /// <summary>
+        /// 得到当前迭代到元素
+        /// </summary>
+        object CurrentItem();
+
+        /// <summary>
+        /// 是否能继续迭代
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        bool MoveNext();
+
+        /// <summary>
+        /// 移动到下一个
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        object Next();
+    }
+
+    /// <summary>
+    /// 自定义的迭代器
+    /// </summary>
+    public class CustomerIterator : Iterator
+    {
+        private readonly List<object> objects;
+
+        private int currentIndex = 0;
+
+        public CustomerIterator(List<object> objects)
+        {
+            this.objects = objects;
+        }
+
+        /// <summary>
+        /// 设置索引器
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public object this[int index]
+        {
+            get { return objects[index]; }
+
+            set { objects[index] = value; }
+        }
+
+        public object First()
+        {
+            if (objects.Count > 0)
+            {
+                return objects[0];
+            }
+
+            throw new Exception("数组长度为0");
+        }
+
+        public object Last()
+        {
+            if (objects.Count > 0)
+            {
+                return objects[objects.Count - 1];
+            }
+
+            throw new Exception("数组长度为0");
+        }
+
+        public object CurrentItem()
+        {
+            return objects[currentIndex];
+        }
+
+        public bool MoveNext()
+        {
+            return currentIndex < objects.Count - 1;
+        }
+
+        public object Next()
+        {
+            return objects[++currentIndex];
+        }
+    }
+}
---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorPattern
+{
+    /// <summary>
+    /// 迭代器模式是最常用的设计模式
+    /// 通常使用的 for  foreach 等都是迭代器模式的使用
+    /// net中的迭代器接口是 IEnumerator
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> arrary = new List<object>
+            {
+                "小明","小红","小刚","小菊"
+            };
+
+            CustomerIterator iterator = new CustomerIterator(arrary);
+            Console.WriteLine($"第一个元素是{iterator.First().ToString()}");
+            Console.WriteLine($"最后一个元素是{iterator.Last().ToString()}");
+
+            while (iterator.MoveNext())
+            {
+                Console.WriteLine($"元素值是{iterator.Next().ToString()}");
+            }
+
+            iterator[2] = "AAA";
+            Console.WriteLine($"修改的元素是{iterator[2]}");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
