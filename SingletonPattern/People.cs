e1362ff7431a40912cd3301ebdd11cdd98f346f3
sl <sunlei@boya-triz.com>
2021-04-21 21:23:01 +0800

    添加单例模式

---
+++ SingletonPattern/People.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class People
+    {
+        private static People _people = null;
+
+        private static readonly object locker = new object();
+
+        //静态构造函数可以避免多线程创建多次实例
+        //static People()
+        //{
+        //    _people = new People();
+        //}
+
+        /// <summary>
+        /// 私有化构造函数 避免外部随便创建实例
+        /// </summary>
+        private People()
+        { }
+
+        /// <summary>
+        /// 获取单例对象
+        /// </summary>
+        /// <returns></returns>
+        public static People GetPeople()
+        {
+            if (_people == null)
+            {
+                _people = new People();
+                Console.WriteLine("单线程创建实例");
+            }
+
+            return _people;
+        }
+
+        /// <summary>
+        /// 多线程获取单例对象
+        /// </summary>
+        /// <returns></returns>
+        public static People GetTaskPeople()
+        {
+            //双If加lock 避免多线程创建多个实例
+            if (_people == null)
+            {
+                lock (locker)
+                {
+                    if (_people == null)
+                    {
+                        _people = new People();
+                        Console.WriteLine("多线程创建实例");
+                    }
+                }
+            }
+
+            return _people;
+        }
+
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    /// <summary>
+    /// 单例模式 
+    /// 应用全局只能使用一个该对象的实例 所以首先需要私有化构造函数 避免外部随便创建实例
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //People p1 = People.GetPeople();
+            //People p2 = People.GetPeople();
+            //Console.WriteLine(object.ReferenceEquals(p1, p2));
+
+            List<Task> tasks = new List<Task>();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    tasks.Add(Task.Run(() =>
+            //    {
+            //        People p = People.GetPeople();
+            //    }));
+            //}
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    People p = People.GetTaskPeople();
+                }));
+            }
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
