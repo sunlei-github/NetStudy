42f978fd4d0f3cc193a170229210f8b9e0dc7d59
sl <sunlei@boya-triz.com>
2021-05-19 20:49:47 +0800

    添加TopShelf

---
+++ TopShelfService/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Topshelf;
+
+namespace TopShelfService
+{
+    /// <summary>
+    /// 使用Topshelf开发Windows服务，比起直接写Windows服务的优势在于，可以直接运行调试。开发完成后，直接使用install命令注册为服务即可，简单高效。
+    /// 官方文档  http://docs.topshelf-project.com/en/latest/configuration/config_api.html 
+    /// 
+    /// 服务安装卸载
+    /// 安装：TopShelfService.exe install
+    ///启动：TopShelfService.exe start
+    ///卸载：TopShelfService.exe uninstall
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(conf =>
+            {
+                conf.SetDisplayName("test_displayname");
+                conf.SetServiceName("test_servicename");
+                conf.SetInstanceName("test_instancename");
+                conf.SetDescription("服务描述");
+
+                conf.SetStartTimeout(TimeSpan.FromSeconds(10));  //设置服务启动超时的时间
+                conf.SetStopTimeout(TimeSpan.FromSeconds(10));  //设置服务停止超时的时间
+
+                conf.Service<TestService>(op =>
+                {
+                    //自定义服务的配置  使用有参数的构造函数
+                    op.ConstructUsing(() => new TestService("服务名称"));
+                    op.WhenStarted((service, host) => service.Start(host));
+                    op.WhenStopped((service, host) => service.Stop(host));
+
+                });  //注册服务   对服务进行自定义配置
+
+                //服务安装 卸载 前后的事件
+                conf.BeforeInstall(c => Console.WriteLine("服务开始安装之前"));
+                conf.AfterInstall(c => Console.WriteLine("服务安装之后"));
+                conf.BeforeUninstall(() => Console.WriteLine("服务卸载之前"));
+                conf.AfterUninstall(() => Console.WriteLine("服务卸载之后"));
+
+                conf.UseLog4Net("log4Net.config");  //使用log4net 
+            });
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TopShelfService/TestService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Topshelf;
+
+namespace TopShelfService
+{
+    public class TestService : ServiceControl
+    {
+
+        public TestService() { }
+
+        public TestService(string name)
+        {
+            Console.WriteLine("使用有参数的构造函数");
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Console.WriteLine("TestService服务已经开始执行");
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Console.WriteLine("TestService服务已经已经停止");
+
+            return true;
+        }
+    }
+}
